6738c15d25a9b5b8ef0cac03b26419095bbf4b1b
Ivonne Vega\PERSONAL <ivonne01101@hotmail.com>
2017-05-20 10:32:47 -0500

    Progra2

--- botonDeber.py
+++ botonDeber.py
@@ -5,7 +5,7 @@ def fun2():
     print('Fernanda Cordova')
 
 def fun3():
-    print('nombre3')
+    print('Ivonne Vega')
 
 def fun4():
     print('nombre4')
@@ -17,7 +17,7 @@ boton1=Button(ventana,text="David Casa",command=fun1)
 boton1.grid(row=1,column=1)
 boton2=Button(ventana,text="Fernanda Cordova",command=fun2)
 boton2.grid(row=1,column=2)
-boton3=Button(ventana,text="Nombre3",command=fun3)
+boton3=Button(ventana,text="Ivonne Vega",command=fun3)
 boton3.grid(row=2,column=1)
 boton4=Button(ventana,text="Nombre4",command=fun4)
 boton4.grid(row=2,column=2)
