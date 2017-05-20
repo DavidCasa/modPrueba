74fa7b62e2d63e7069b3a1304e02a7bfd90f1d44
Fabricio\Jhon <maria.cordova01@epn.edu.ec>
2017-05-20 10:22:15 -0500

    edicion

--- botonDeber.py
+++ botonDeber.py
@@ -2,7 +2,7 @@ def fun1():
     print('David Casa')
 
 def fun2():
-    print('nombre2')
+    print('Fernanda Cordova')
 
 def fun3():
     print('nombre3')
@@ -15,7 +15,7 @@ ventana = Tk()
 ventana.title('Deber Botones')
 boton1=Button(ventana,text="David Casa",command=fun1)
 boton1.grid(row=1,column=1)
-boton2=Button(ventana,text="Nombre2",command=fun2)
+boton2=Button(ventana,text="Fernanda Cordova",command=fun2)
 boton2.grid(row=1,column=2)
 boton3=Button(ventana,text="Nombre3",command=fun3)
 boton3.grid(row=2,column=1)
